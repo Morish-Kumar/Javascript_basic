fb83d62ca06d55a7bd3b378c29e0fa1fa312f8bb
singh-morish <morish.dtg@gmail.com>
2023-08-11 22:07:11 +0530

    adding array and function code

---
+++ jsArray.js
@@ -0,0 +1,11 @@
+// every array in JS is a object
+
+let color = ["red", "blue"]
+console.log(color)
+console.log(color[0])
+
+
+color[2] = "green"
+console.log(color)
+
+console.log(color.length)
---
+++ jsfunction.js
@@ -0,0 +1,19 @@
+/**
+ // default function
+ function greet(){
+     console.log("Hello World");
+ }
+ 
+ greet()
+
+ */
+
+/**
+ //  function with argumnent
+ function greet(name){
+     console.log("Hello " + name);
+ }
+ 
+ greet("John")
+
+ */
